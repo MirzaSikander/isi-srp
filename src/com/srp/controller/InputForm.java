9f92c9bced3f443f90587f0d250ce6b07f4d5794
aswinasar <aswinasar@yahoo.com>
2014-04-13 18:31:57 -0700

    Test Commit

--- src/com/srp/controller/InputForm.java
+++ src/com/srp/controller/InputForm.java
@@ -21,6 +21,11 @@ import com.srp.model.*;
 public class InputForm extends HttpServlet {
 
     /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
      *
