ef3b7187eb84e4434fed8e8a3e245b9ec509ba62
aswinasar <aswinasar@yahoo.com>
2014-04-13 19:22:15 -0700

    Database connectivity issue resolved.

--- src/com/srp/model/DatabaseConnection.java
+++ src/com/srp/model/DatabaseConnection.java
@@ -46,6 +46,16 @@ public class DatabaseConnection {
         }
     }
     
+    public DatabaseConnection() {
+    	try {
+    	    System.out.println("Loading driver...");
+    	    Class.forName("com.mysql.jdbc.Driver");
+    	    System.out.println("Driver loaded!");
+    	} catch (ClassNotFoundException e) {
+    	    throw new RuntimeException("Cannot find the driver in the classpath!", e);
+    	}
+    }
+    
     public int rand() {
         int i = (int) (Math.random()*1000*Math.random());
         return i;
